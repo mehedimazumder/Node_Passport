9e1cc6be34c49f9d0bfbf924e6195ee3c89771b8
mehedi <mehedirumc12cuet14@gmail.com>
2019-11-26 17:16:05 +0600

    reg log dash ejs added

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 //login page
-router.get('/login', (req, res, next) => res.send('login'));
+router.get('/login', (req, res, next) => res.render('login'));
 //register page
-router.get('/register', (req, res, next) => res.send('reg'));
+router.get('/register', (req, res, next) => res.render('register'));
 
 module.exports = router;
